dd62561020a66e8815bdb3bb1310d2ad944a7d5e
Wilmar Garcia <wcgarcia5@gmail.com>
2025-01-19 15:36:31 -0500

    changes to create test user from docker file -- problem with instance

--- create_user.sh
+++ create_user.sh
@@ -5,9 +5,15 @@ USERNAME="seek_test"
 EMAIL="seek_test@example.com"
 PASSWORD="seek_password"
 
-# Create the user if it doesn't already exist
-echo "Creating user $USERNAME if it doesn't exist..."
+echo "Checking if the user '$USERNAME' exists..."
 python manage.py shell <<EOF
 from django.contrib.auth.models import User
-if not User.objects.filter(username="$USERNAME").exists():
-    User.objects.create_user(username="$USERNAME", email="$EMAIL", password="$PASSWORD"
+try:
+    if not User.objects.filter(username="$USERNAME").exists():
+        User.objects.create_user(username="$USERNAME", email="$EMAIL", password="$PASSWORD")
+        print("User '$USERNAME' created successfully.")
+    else:
+        print("User '$USERNAME' already exists.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+EOF
